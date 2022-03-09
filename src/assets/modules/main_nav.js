d63b3bd858ffc1e8929c7b3102e6e928d683f0b8
Kolpsi <tykataf@yandex.ru>
2022-03-09 15:40:19 +0500

    Начало работы над гамбургер меню

---
+++ src/assets/modules/main_nav.js
@@ -0,0 +1,19 @@
+(function () {
+    const navMain = document.querySelector('.main-nav');
+    const navToggle = document.querySelector('.main-nav__toggle');
+
+
+    navMain.classList.remove('main-nav--nojs');
+    /**
+     * @description Функция переключения навигационного меню
+     */
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    });
+  })();
