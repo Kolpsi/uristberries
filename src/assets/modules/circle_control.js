55d7f7c30b610adb1dd92aad9300e372d3aa1947
Kolpsi <tykataf@gmail.com>
2022-03-13 15:19:16 +0500

    Блок карточек

--- src/assets/modules/circle_control.js
+++ src/assets/modules/circle_control.js
@@ -1,3 +1,15 @@
 // создание круга
-document.querySelector('.circle').innerHTML = "листать вниз - листать вниз - листать вниз".split('')
-    .map((e, i) => `<span class="circle-span" style="--rot:${i*12}deg">${e}</span>`).join('');
+// document.querySelector('.circle').innerHTML = "листать вниз - листать вниз - листать вниз".split('')
+//     .map((e, i) => `<span class="circle-span" style="--rot:${i*12}deg">${e}</span>`).join('');
+
+
+    function createCircle (circle, circleText, deg) {
+        document.querySelector(circle).innerHTML = circleText.split('')
+    .map((e, i) => `<span class="circle-span" style="--rot:${i*deg}deg">${e}</span>`).join('');
+
+    }
+
+    createCircle('.circle', 'листать вниз - листать вниз - листать вниз', 12)
+    createCircle('.circle-nabotov', 'оперативное и стратегическое управление компанией', 7.34)
+    createCircle('.circle-astahov', 'руководство отделом юридической защиты', 9,47)
+    createCircle('.circle-kapustina', 'координация работы юридического отдела', 9,47)
