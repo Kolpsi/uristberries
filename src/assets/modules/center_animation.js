e7c36a279ccfc1d4b3b31268170e14f4942736d9
Kolpsi <tykataf@yandex.ru>
2022-03-15 16:13:19 +0500

    Слайдер happy

--- src/assets/modules/center_animation.js
+++ src/assets/modules/center_animation.js
@@ -2,46 +2,28 @@
     const adviseItem1 = document.querySelector('.advice-item-1');
     const adviseItem2 = document.querySelector('.advice-item-2');
     const adviseItem3 = document.querySelector('.advice-item-3');
-    const adviseTitle = document.querySelector('.advice-title')
-    if (document.documentElement.clientWidth < 1160) {
-        function getCoord(elem) {
-            let centerElem = document.elementFromPoint(400, 400)
-            if (centerElem.classList.contains('advice-item-1')) {
-                adviseItem1.classList.add('active');
-                adviseItem2.classList.remove('active');
-                adviseItem3.classList.remove('active');
-            } else
-            if (centerElem.classList.contains('advice-item-2')) {
-                adviseItem2.classList.add('active');
-                adviseItem1.classList.remove('active');
-                adviseItem3.classList.remove('active');
-            } else
-            if (centerElem.classList.contains('advice-item-3')) {
-                adviseItem3.classList.add('active');
-                adviseItem1.classList.remove('active');
-                adviseItem2.classList.remove('active');
-            }
-        };
-        document.addEventListener('scroll', getCoord)
-    } else {
-        function getCoord(elem) {
-            let centerElem = document.elementFromPoint(1170, 300)
-            if (centerElem.classList.contains('advice-item-1')) {
-                adviseItem1.classList.add('active');
-                adviseItem2.classList.remove('active');
-                adviseItem3.classList.remove('active');
-            } else
-            if (centerElem.classList.contains('advice-item-2')) {
-                adviseItem2.classList.add('active');
-                adviseItem1.classList.remove('active');
-                adviseItem3.classList.remove('active');
-            } else
-            if (centerElem.classList.contains('advice-item-3')) {
-                adviseItem3.classList.add('active');
-                adviseItem1.classList.remove('active');
-                adviseItem2.classList.remove('active');
-            }
-        };
-        document.addEventListener('scroll', getCoord)
+
+    function getCoord(elem) {
+        let centerX = document.documentElement.clientWidth / 2;
+        let centerY = document.documentElement.clientHeight / 2;
+        let centerElem = document.elementFromPoint(centerX, centerY)
+        if (centerElem.classList.contains('advice-item-1')) {
+            adviseItem1.classList.add('active');
+            adviseItem2.classList.remove('active');
+            adviseItem3.classList.remove('active');
+        } else
+        if (centerElem.classList.contains('advice-item-2')) {
+            adviseItem2.classList.add('active');
+            adviseItem1.classList.remove('active');
+            adviseItem3.classList.remove('active');
+        } else
+        if (centerElem.classList.contains('advice-item-3')) {
+            adviseItem3.classList.add('active');
+            adviseItem1.classList.remove('active');
+            adviseItem2.classList.remove('active');
+        }
     };
+
+    document.addEventListener('scroll', getCoord)
+
 })();
--- src/assets/modules/swiper.js
+++ src/assets/modules/swiper.js
@@ -15,16 +15,28 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
-// if (document.documentElement.clientWidth > 1160) {
-//     // const happy = document.querySelector('.happy');
-//     // const happylist = happy.querySelector(".happy-list");
-//     // const swiperContainer = happy.querySelector(".swiper-container");
-//     // const happyItem = happy.querySelectorAll('.happy-item');
-//     // happylist.classList.add('swiper2')
-//     // swiperContainer.classList.add('swiper-wrapper')
-//     const swiper2 = new Swiper('.swiper2', {
-//         loop: true,
-//         slidesPerView: 1,
-//         centeredSlides: true,
-//     });
-// }
+if (document.documentElement.clientWidth > 1160) {
+    const happyList = document.querySelector('.happy-list');
+    const happyItem = document.querySelectorAll('.happy-item');
+
+    happyItem.forEach((item) => {
+        item.classList.add('swiper-slide');
+        item.classList.remove('happy-item-hidden')
+    })
+
+    happyList.classList.add('swiper-wrapper');
+
+    var x = document.getElementsByClassName("slider-produtos-wrap");
+
+
+    const swiper2 = new Swiper('.swiper2', {
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: 600,
+        centeredSlides: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
