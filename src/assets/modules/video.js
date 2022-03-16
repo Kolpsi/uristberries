135b1ce513055df8b8dc629c9e34d339c562a759
Kolpsi <tykataf@yandex.ru>
2022-03-16 13:54:28 +0500

    Форматирование js файлов

--- src/assets/modules/video.js
+++ src/assets/modules/video.js
@@ -1,27 +1,28 @@
 // Модуль констроля видео
+(function() {
+    // объявление кнопки плей
+    $('.about-video').parent().click(function() {
+        if ($(this).children(".about-video").get(0).paused) {
+            $(this).children(".about-video").get(0).play();
+            $(this).children(".playpause").fadeOut();
 
-// объявление кнопки плей
-$('.about-video').parent().click(function() {
-    if ($(this).children(".about-video").get(0).paused) {
-        $(this).children(".about-video").get(0).play();
-        $(this).children(".playpause").fadeOut();
+        } else {
+            $(this).children(".about-video").get(0).pause();
 
+            $(this).children(".playpause").fadeIn();
+
+        }
+
+
+    });
+
+    const videoWrapper = document.querySelector('.video-wrapper');
+    const video = videoWrapper.querySelector('.about-video');
+
+    // если нет ссылки видео не отображается
+    if (video.src == '') {
+        videoWrapper.classList.add('hidden');
     } else {
-        $(this).children(".about-video").get(0).pause();
-
-        $(this).children(".playpause").fadeIn();
-
+        videoWrapper.classList.add('show');
     }
-
-
-});
-
-const videoWrapper = document.querySelector('.video-wrapper');
-const video = videoWrapper.querySelector('.about-video');
-
-// если нет ссылки видео не отображается
-if (video.src == '') {
-    videoWrapper.classList.add('hidden');
-} else {
-    videoWrapper.classList.add('show');
-}
+})();
