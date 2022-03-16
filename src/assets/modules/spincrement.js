01f63f4b134ee874780336a00f9f75129a46ab06
Kolpsi <tykataf@yandex.ru>
2022-03-16 14:14:30 +0500

    error on js

--- src/assets/modules/spincrement.js
+++ src/assets/modules/spincrement.js
@@ -1,17 +1,15 @@
 // Анимация счетчика
-$(function() {
-    var Animation = function() {
-        var hFrom = $(window).scrollTop();
-        var hTo = $(window).scrollTop() + ($(window).height() / 3);
-        if ($(".statistic-list").offset().top > hFrom && $(".statistic-list").offset().top < hTo) {
-            $('.statistic-item-title').delay(500).spincrement({
-                thousandSeparator: "",
-                duration: 3000
-            });
-        }
-    };
-    $(window).scroll(function() {
-        Animation();
-    });
+var Animation = function() {
+    var hFrom = $(window).scrollTop();
+    var hTo = $(window).scrollTop() + ($(window).height() / 3);
+    if ($(".statistic-list").offset().top > hFrom && $(".statistic-list").offset().top < hTo) {
+        $('.statistic-item-title').delay(500).spincrement({
+            thousandSeparator: "",
+            duration: 3000
+        });
+    }
+};
+$(window).scroll(function() {
     Animation();
-})();
+});
+Animation();
